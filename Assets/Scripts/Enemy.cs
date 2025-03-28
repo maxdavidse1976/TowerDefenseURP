e74ffabbd1f556875b3a947337be28bde7fdb7f4
Max Davidse <m.davidse@rubicon.nl>
2025-03-28 22:16:51 +0100

    Added the use of multiple waypoints to improve the navigational routes they take and added more enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,22 +3,33 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] Transform _waypoint;
+    [SerializeField] Transform[] _waypoints;
 
+    int _waypointIndex;
     NavMeshAgent _agent;
 
     void Awake()
     {
         _agent = GetComponent<NavMeshAgent>();
     }
-    void Start()
-    {
-        _agent.SetDestination(_waypoint.position);
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (_agent.remainingDistance < .5f)
+        {
+            _agent.SetDestination(GetNextWaypoint());
+        }
+    }
+
+    Vector3 GetNextWaypoint()
+    {
+        if (_waypointIndex >= _waypoints.Length)
+        {
+            return transform.position;
+        }
+
+        Vector3 targetPoint = _waypoints[ _waypointIndex].position;
+        _waypointIndex++;
+
+        return targetPoint;
     }
 }
