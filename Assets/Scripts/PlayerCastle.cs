29790da3bc83416040505e0e851137384c89bf7a
Max Davidse <m.davidse@rubicon.nl>
2025-03-31 21:14:48 +0200

    Added a player castle, and added colliders etc, to make enemies die when they enter the castle gate

---
+++ Assets/Scripts/PlayerCastle.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class PlayerCastle : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
